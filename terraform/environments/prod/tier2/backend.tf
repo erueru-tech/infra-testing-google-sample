be6e9a0c1fab5af3cdeff59cad21892f9ecfe6bf
fittecs <jun.fujita@fittecs.com>
2024-04-05 01:58:24 +0900

    HashiCorpのスタイルガイドに対応

--- terraform/environments/prod/tier2/backend.tf
+++ terraform/environments/prod/tier2/backend.tf
@@ -7,9 +7,9 @@ terraform {
     # prod環境についてはローカルから誤って、destroyコマンドを発行しないよう意図的にバケット名をそのままや適当な名前にして、
     # 本番リリースCI/CD時に-backend-configオプションでstate管理バケットを指定するような運用を想定している
     # sandbox環境は直書きしても問題ないが、自分用の環境のバケット設定を維持するためにこのフォルダ内の.gitignoreに
-    # terraform.tf(このファイル)を指定するなどしてGit管理されないようにする
+    # backend.tf(このファイル)を指定するなどしてGit管理されないようにする
 
-    bucket = "your-terraform-bucket-name"
-    prefix = "terraform/tier1-state"
+    bucket  = "your-terraform-bucket-name"
+    prefix  = "terraform/tier2-state"
   }
 }
