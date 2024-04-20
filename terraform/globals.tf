e9fb7c90d7c4e7073d888e2a68005f0899cbe9da
fittecs <jun.fujita@fittecs.com>
2024-04-20 17:57:26 +0900

    storageモジュールの削除、プロバイダおよびterraformバージョンのアップグレード

--- terraform/globals.tf
+++ terraform/globals.tf
@@ -4,18 +4,18 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.19.0"
+      version = "5.25.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "5.19.0"
+      version = "5.25.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.6.0"
+      version = "3.6.1"
     }
   }
-  required_version = "1.7.5"
+  required_version = "1.8.1"
 }
 
 provider "google" {
--- terraform/modules/storage/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "sample_bucket_name" {
-  value = google_storage_bucket.sample.name
-}
